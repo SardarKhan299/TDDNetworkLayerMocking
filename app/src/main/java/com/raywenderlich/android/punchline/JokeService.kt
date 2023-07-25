4416f980fdc6ddd0ef926a9985f65c9642d920cd
SardarkhanStella <skhan@stellatechnology.com>
2023-07-25 15:27:13 +0500

    Add Get method to pass the test.

--- app/src/main/java/com/raywenderlich/android/punchline/JokeService.kt
+++ app/src/main/java/com/raywenderlich/android/punchline/JokeService.kt
@@ -31,7 +31,9 @@
 package com.raywenderlich.android.punchline
 
 import io.reactivex.Single
+import retrofit2.http.GET
 
 interface JokeService {
+    @GET("https://raywenderlich.com")
     fun getRandomJoke(): Single<Joke>
 }
