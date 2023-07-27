6c884dc55abd53b06e78c8ad7d12e703ead73afc
SardarkhanStella <skhan@stellatechnology.com>
2023-07-27 17:07:09 +0500

    Make a failing test for newJoke Button to click and get a new joke.

--- app/src/androidTest/java/com/raywenderlich/android/punchline/MainActivityTest.kt
+++ app/src/androidTest/java/com/raywenderlich/android/punchline/MainActivityTest.kt
@@ -2,6 +2,7 @@ package com.raywenderlich.android.punchline
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -15,7 +16,9 @@ import org.koin.test.inject
 import org.koin.test.mock.MockProviderRule
 import org.koin.test.mock.declareMock
 import org.mockito.Mockito
+import org.mockito.invocation.InvocationOnMock
 import org.mockito.kotlin.whenever
+import org.mockito.stubbing.Answer
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest:KoinTest {
@@ -54,6 +57,39 @@ class MainActivityTest:KoinTest {
 
         onView(withId(R.id.textJoke))
             .check(matches(withText(joke.joke)))
+    }
+
+
+    @Test
+    fun onButtonClickNewJokeIsDisplayed(){
+        val joke = Joke(faker.idNumber().valid(),faker.lorem().sentence())
+        val jokeQueueAnswer = object : Answer<Single<Joke>>{
+            val jokes = listOf(
+                Joke(
+                    faker.idNumber().valid(),
+                    faker.lorem().sentence()),
+                joke
+            )
+            var currentJoke = -1
+            override fun answer(invocation: InvocationOnMock?): Single<Joke> {
+                currentJoke++
+                return Single.just(jokes[currentJoke])
+            }
+        }
+
+        declareMock<Repository> {
+            whenever(getJoke())
+                .thenAnswer(jokeQueueAnswer)
+        }
+        ActivityScenario.launch(MainActivity::class.java)
+
+        // 1
+        onView(withId(R.id.buttonNewJoke))
+            .perform(click())
+        // 2
+        onView(withId(R.id.textJoke))
+            .check(matches(withText(joke.joke)))
+
     }
 
 }
